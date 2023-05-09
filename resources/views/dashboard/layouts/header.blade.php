a4331e237e676d09cece6ef4848d17b9f6acc264
rahmahkp <kusumarahmah2@gmail.com>
2023-05-10 01:02:25 +0700

    Update
    Mengubah dan menambahkan beberapa style  dan html

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -2,15 +2,15 @@
     <div id="page-content-wrapper">
         <nav class="navbar navbar-expand-lg" style="color: white;">
             <div class="d-flex align-items-center">
-                <i class="fas fa-bars fs-4 me-3" id="menu-toggle" style="margin-left: 20px; margin-bottom: 40px;"></i>
-                <h2 class="fw-bold" style="font-size: 25px; color: white; font-weight: 300; margin-top: -40px;">PANDAY</h2>
+                <i class="fas fa-bars fs-4 me-3" id="menu-toggle" style="margin-left: 20px; margin-bottom: 35px;"></i>
+                <h2 class="fw-bold" style="font-size: 25px; color: white; font-weight: 300; margin: -40px 0px -3px 0;">PANDAY</h2>
             </div>
 
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 mb-sm-2">
-                <li class="nav-item dropdown" style="margin-right: 40px; margin-bottom: 40px;">
-                    <a class="nav-link text-white" href="#">
-                        <i class="fas fa-user me-2"></i>{{ $user->name }}
-                    </a>
+                <li class="nav-item" style="margin-right: 40px;">
+                    <span class="text-white">
+                        <i class="fas fa-user me-2" style="margin-bottom: 35px;"></i>{{ $user->name }}
+                    </span>
                 </li>
             </ul>
         </nav>
--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -24,7 +24,7 @@
     </div> <!-- /d-flex -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/dashboard/beranda.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
 </html>
