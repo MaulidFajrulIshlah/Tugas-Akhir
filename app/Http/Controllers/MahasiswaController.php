cce5896ef75e28436fab37eb7a21f1df3b1e1e38
rahmahkp <kusumarahmah2@gmail.com>
2023-06-20 12:01:55 +0700

    Add and Update Mahasiswa
    Menambah dan mengubah view blade fitur mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -13,7 +13,7 @@ class MahasiswaController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         // mendapatkan data fakultas
         $fakultas = Fakultas::all();
@@ -21,13 +21,50 @@ class MahasiswaController extends Controller
         // mendapatkan data prodi
         $prodi = Prodi::with('fakultas')->get();
 
-        $user = Auth::user();
-        if ($user->id_jabatan === 1) {
-            return view('dashboard.admin.mahasiswa', compact('fakultas', 'prodi'));
-        } else if ($user->id_jabatan === 2 || $user->id_jabatan === 3 || $user->id_jabatan === 4) {
-            return view('dashboard.dekanat_tendik.mahasiswa', compact('fakultas', 'prodi'));
-        } else if ($user->id_jabatan === 5 || $user->id_jabatan === 6) {
-            return view('dashboard.prodi.mahasiswa', compact('fakultas', 'prodi'));
+        // Cek apakah parameter unitID ada dalam request
+        if ($request->has('unitID')) {
+            $unitID = $request->input('unitID');
+            // Lakukan penanganan berdasarkan unitID
+            if ($unitID == 1) {
+                return view('dashboard.aksesLayar.pasca_kenotariatan_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 2) {
+                return view('dashboard.aksesLayar.pasca_manajemen_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 3) {
+                return view('dashboard.aksesLayar.pasca_sainsBio_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 4) {
+                return view('dashboard.aksesLayar.pasca_adminRS_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 5) {
+                return view('dashboard.aksesLayar.pasca_doktor_sainsBio_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 6) {
+                return view('dashboard.aksesLayar.fk_kedokteran_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 7) {
+                return view('dashboard.aksesLayar.fk_profesi_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 8) {
+                return view('dashboard.aksesLayar.fkg_kg_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 9) {
+                return view('dashboard.aksesLayar.fti_ti_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 10) {
+                return view('dashboard.aksesLayar.fti_ip_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 11) {
+                return view('dashboard.aksesLayar.feb_manajemen_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 12) {
+                return view('dashboard.aksesLayar.feb_akuntansi_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 13) {
+                return view('dashboard.aksesLayar.fh_hukum_table', compact('fakultas', 'prodi'));
+            } else if ($unitID == 14) {
+                return view('dashboard.aksesLayar.fpsi_psi_table', compact('fakultas', 'prodi'));
+            }
         }
+
+        // Jika unitID == null
+        return view('dashboard.mahasiswa', compact('fakultas', 'prodi'));
+
+        // if ($user->id_jabatan === 1) {
+        //     return view('dashboard.admin.mahasiswa', compact('fakultas', 'prodi'));
+        // } else if ($user->id_jabatan === 2 || $user->id_jabatan === 3 || $user->id_jabatan === 4) {
+        //     return view('dashboard.dekanat_tendik.mahasiswa', compact('fakultas', 'prodi'));
+        // } else if ($user->id_jabatan === 5 || $user->id_jabatan === 6) {
+        //     return view('dashboard.prodi.mahasiswa', compact('fakultas', 'prodi'));
+        // }
     }
 }
---
+++ resources/views/dashboard/mahasiswa.blade.php
@@ -0,0 +1,36 @@
+@extends('dashboard.layouts.main')
+@section ('title', 'PANDAY | Mahasiswa')
+@section('content')
+    <!-- Route -->
+    <h5 class="fw-bold" style="margin-top: 40px; font-weight: 400;">Mahasiswa</h5>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('beranda') }}">Dashboard</a></li>
+        <li class="breadcrumb-item active">Mahasiswa</li>
+    </ol>
+
+    <div class="row g-3 my-3">
+        <div class="col mx-2 bg-white rounded card content" id="wrapper-content">
+            <h5 class="m-3 mt-4 mb-0 fw-bold text">Fakultas dan Prodi Universitas YARSI</h5>
+            <hr>
+            <div class="row g-0 mb-2">
+                <div class="row">
+                    <ul class="semester mb-2">
+                        @foreach ($prodi as $prodis)
+                            <li class="link">
+                                <a href="{{ route('mahasiswa', ['unitID' => $prodis->id]) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>{{ $prodis->fakultas->nama_fakultas }} / {{ $prodis->nama_prodi }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div> <!-- /col bg-white -->
+    </div>
+@endsection
+        {{-- <script>
+           // Menangkap event saat halaman di-refresh
+            window.onload = function() {
+                if (window.location.search.includes('unitID=')) {
+                    window.history.replaceState({}, document.title, "{{ route('mahasiswa') }}");
+                }
+            }
+        </script> --}}
