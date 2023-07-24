0f594e6c9497972499cdfd2bee428a912f159d92
rahmahkp <kusumarahmah2@gmail.com>
2023-07-24 10:27:36 +0700

    Update dataMonitoring FTI

---
+++ resources/views/dashboard/dataMonitoring/fti/ti/2020_2021_antara.blade.php
@@ -0,0 +1,62 @@
+@extends('dashboard.layouts.main')
+@section('title', 'PANDAY | Mata Kuliah')
+@section('content')
+    <!-- Route -->
+    <h5 class="fw-bold" style="margin-top: 40px; font-weight: 400;">Data Monitoring Akademik</h5>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('beranda') }}">Dashboard</a></li>
+        <li class="breadcrumb-item active">Data Monitoring Akademik</li>
+        <li class="breadcrumb-item active">Fakultas Teknologi Informasi</li>
+        <li class="breadcrumb-item active">Teknik Informatika</li>
+        <li class="breadcrumb-item active">2020_2021 Antara</li>
+    </ol>
+
+    <div class="row g-3 my-3">
+        <div class="col mx-2 bg-white rounded card content" id="wrapper-content">
+            <div class="row g-0 my-3">
+                <div class="row mb-4">
+                    <h5 class="mb-2 fw-bold text">Data Monitoring Akademik</h5>
+                    <span class="fs-6 mb-3 text">Semester 2020/2021 Antara - Teknik Informatika</span>
+                    {{-- Table --}}
+                    <div class="container mt-3">
+                        <table id="data-matkul" class="table table-bordered table-hover cell-border">
+                            <thead class="table-success">
+                                <tr>
+                                    <th rowspan="2" scope="col" class="text" style="text-align: center;">No</th>
+                                    <th rowspan="2" scope="col" class="text" style="text-align: center;">Nama Mata Kuliah</th>
+                                    <th colspan="2" scope="col" class="text" style="text-align: center;">Pengumpulan</th>
+                                    <th colspan="16" scope="col" class="text merged-cell" style="text-align: center;">Kegiatan Belajar</th>
+                                </tr>
+                                <tr>
+                                    <th style="text-align: center;">Tugas</th>
+                                    <th style="text-align: center;">Kuis</th>
+                                    <th style="text-align: center;">P1</th>
+                                    <th style="text-align: center;">P2</th>
+                                    <th style="text-align: center;">P3</th>
+                                    <th style="text-align: center;">P4</th>
+                                    <th style="text-align: center;">P5</th>
+                                    <th style="text-align: center;">P6</th>
+                                    <th style="text-align: center;">P7</th>
+                                    <th style="text-align: center;">P7</th>
+                                    <th style="text-align: center;">P9</th>
+                                    <th style="text-align: center;">P10</th>
+                                    <th style="text-align: center;">P11</th>
+                                    <th style="text-align: center;">P12</th>
+                                    <th style="text-align: center;">P13</th>
+                                    <th style="text-align: center;">P14</th>
+                                    <th style="text-align: center;">P15</th>
+                                    <th style="text-align: center;">P16</th>
+                                </tr>
+                            </thead>
+
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="{{ asset('js/dataMonitoring/fti/ti/2020_2021_antara.js') }}"></script>
+
+@endsection
---
+++ resources/views/dashboard/semester/fti/ti/2022_2023_antara.blade.php
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    const table = $('#data-matkul').DataTable();
+});@extends('dashboard.layouts.main')
+@section('title', 'PANDAY | Mata Kuliah')
+@section('content')
+    <!-- Route -->
+    <h5 class="fw-bold" style="margin-top: 40px; font-weight: 400;">Mata Kuliah</h5>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('beranda') }}">Dashboard</a></li>
+        <li class="breadcrumb-item active">Mata Kuliah</li>
+        <li class="breadcrumb-item active">Fakultas Teknologi Informasi</li>
+        <li class="breadcrumb-item active">Teknik Informatika</li>
+        <li class="breadcrumb-item active">2022_2023 Antara</li>
+    </ol>
+
+    <div class="row g-3 my-3">
+        <div class="col mx-2 bg-white rounded card content" id="wrapper-content">
+
+            <div class="row g-0 my-3">
+                <div class="row mb-4">
+                    <h5 class="mb-2 fw-bold text">Mata Kuliah</h5>
+                    <span class="fs-6 mb-3 text">Semester 2022/2023 Antara - Teknik Informatika</span>
+
+                    <div class="col-xl-4 col-md-6 col-11 col-lg-5 my-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="ps-1">
+                                    <div class="header">
+                                        <h5 class="card-title fw-bold">Jumlah Daftar Mata Kuliah</h5>
+                                    </div>
+                                    <h5 class="fw-bold mt-3" id="jumlah-matkul"></h5>
+                                    <h6 class="small pt-1">Yang terdapat di LAYAR</h6>
+                                    <div class="card-footer bg-transparent mt-3 ps-0">
+                                        <small class="text-danger"><span id="last-updated"></span></small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container mt-4">
+                        <table  id="data-matkul" table class="table table-bordered table-hover cell-border">
+                            <thead class="table-success">
+                                <tr>
+                                    <th scope="col" class="text">No</th>
+                                    <th scope="col" class="text">Daftar Mata Kuliah</th>
+                                    <th scope="col" class="text">Halaman Mata Kuliah Lengkap</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/semester/fti/ti/2022_2023_antara.js') }}"></script>
+
+@endsection
