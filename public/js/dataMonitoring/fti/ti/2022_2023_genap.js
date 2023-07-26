4449b8a3527c928cba943dd454af2ccaf6c12730
intanmlnds <80166122+intanmlnds@users.noreply.github.com>
2023-07-26 23:46:26 +0700

    Update dataMonitoring FTI

--- public/js/dataMonitoring/fti/ti/2022_2023_genap.js
+++ public/js/dataMonitoring/fti/ti/2022_2023_genap.js
@@ -46,16 +46,15 @@ async function updateData() {
 
       // Menghitung jumlah kegiatan belajar per pertemuan
       resources.resources.forEach((resource) => {
-        // Mendapatkan nomor pertemuan dari nama file (mengasumsikan format nama file: "P1.pdf", "P2.doc", dll)
-        const pertemuan = parseInt(resource.name.replace(/\D/g, ''));
-        if (!isNaN(pertemuan) && pertemuan >= 1 && pertemuan <= 16) {
-          kegiatanPerPertemuan[pertemuan - 1]++;
+        const section = parseInt(resource.section); // Mengambil nomor section dari atribut 'section'
+        if (!isNaN(section) && section >= 1 && section <= 16) {
+          kegiatanPerPertemuan[section - 1]++;
         } else if (assignments.warnings && assignments.warnings.length > 0) {
           const warning = assignments.warnings[0];
           if (warning.message === "User is not enrolled or does not have requested capability") {
-            jumlahPengumpulan = "User is not enrolled";
+              jumlahPengumpulan = "User is not enrolled";
           }
-        }
+      }
       });
 
       matkul.push({
@@ -112,7 +111,7 @@ async function updateData() {
   });
 }
 
-$(document).ready(function () {
-  const table = $('#data-matkul').DataTable();
-  updateData();
-});
+    $(document).ready(function () {
+        const table = $('#data-matkul').DataTable();
+        updateData();
+    });
