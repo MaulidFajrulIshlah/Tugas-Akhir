24b6f08b92f3374cb68931d7243bc37afcf43ef1
rahmahkp <kusumarahmah2@gmail.com>
2023-07-30 12:04:23 +0700

    Update title

--- resources/views/dashboard/dataMonitoring/fpsi/semester.blade.php
+++ resources/views/dashboard/dataMonitoring/fpsi/semester.blade.php
@@ -1,13 +1,13 @@
 @extends('dashboard.layouts.main')
-@section ('title', 'PANDAY | Mata Kuliah')
+@section ('title', 'PANDAY | Akademik')
 @section('content')
     <!-- Route -->
     <h5 class="fw-bold" style="margin-top: 40px; font-weight: 400;">Akademik</h5>
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{ route('beranda') }}">Dashboard</a></li>
         <li class="breadcrumb-item active">Akademik</li>
-        <li class="breadcrumb-item active">Fakultas Teknologi Informasi</li>
-        <li class="breadcrumb-item active">Ilmu Perpustakaan</li>
+        <li class="breadcrumb-item active">Fakultas Psikologi</li>
+        <li class="breadcrumb-item active">Psikologi</li>
     </ol>
 
     <div class="row g-3 my-3">
@@ -15,40 +15,14 @@
             <h5 class="m-3 mt-4 mb-0 fw-bold text">Daftar Semester</h5>
             <hr>
             
-        <div class="row g-0">
-            <div class="row">
-                <ul class="semester">
-                    @foreach ($prodi as $prodis)
-                        @if($prodis->id == 15)
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '14']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2019/2020 Ganjil</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '37']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2019/2020 Genap</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '68']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2020/2021 Ganjil</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '184']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2020/2021 Genap</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '265']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2021/2022 Ganjil</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '357']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2021/2022 Genap</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '427']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2022/2023 Ganjil</a>
-                            </li>
-                            <li class="link">
-                                <a href="{{ route('akademik', ['unitID' => $prodis->id, 'categoryID' => '489']) }}" class="text fs-5 text-center"><i class="fas fa-caret-right m-3"></i>2022/2023 Genap</a>
-                            </li>
-                        @endif
-                    @endforeach
-                </ul>
+            <div class="row g-0">
+                <div class="row">
+                    <ul id="semester" class="fakultas-prodi"></ul>
+                </div>
             </div>
-        </div>
         </div> <!-- /col bg-white -->
     </div>
+
+    <script src="{{ asset('js/dataMonitoring/fpsi/semester.js') }}"></script>
+
 @endsection
