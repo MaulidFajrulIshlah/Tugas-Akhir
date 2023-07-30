93de435a8b676d0a39f13c997e3c59e2083b3b52
rahmahkp <kusumarahmah2@gmail.com>
2023-07-30 23:10:09 +0700

    Delete Suspend dan Jumlah Akun

--- public/js/daftarMatkul/admin.js
+++ public/js/daftarMatkul/admin.js
@@ -1,109 +1,63 @@
 function updateData() {
- 
-    const matkul = [];
-    $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: 'https://layar.yarsi.ac.id/webservice/rest/server.php?wstoken=463cfb78c5acc92fbed0656c2aec27b4&wsfunction=core_course_get_courses&moodlewsrestformat=json',
-        cache: true,
 
-        success: function (data, status, xhr) {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i]['categoryid'] !== null) {
-                    const namaMatkul = data[i]['fullname'];
-                    matkul.push(namaMatkul);
-                }
-            }
-            $('#jumlah-matkul').text(matkul.length);
+  const matkul = [];
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: 'https://layar.yarsi.ac.id/webservice/rest/server.php?wstoken=463cfb78c5acc92fbed0656c2aec27b4&wsfunction=core_course_get_courses&moodlewsrestformat=json',
+    cache: true,
 
-            updateTime();
-
-        },
-        
-        error: function (xhr, status, error) {
-            // penanganan kesalahan saat permintaan AJAX gagal
-            console.log('Error:', error);
+    success: function (data, status, xhr) {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i]['categoryid'] !== null) {
+          const namaMatkul = data[i]['fullname'];
+          matkul.push(namaMatkul);
         }
-    });
-}
-
-function updateAkun() {
-    const users = [];
-    let i = 1;
-    let boo = true;
-    let nomor = 1;
-    
-    while (boo) {
-      $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: 'https://layar.yarsi.ac.id/webservice/rest/server.php?wstoken=463cfb78c5acc92fbed0656c2aec27b4&wsfunction=core_user_get_users_by_field&moodlewsrestformat=json&field=id&values[0]=' + i,
-        async: false,
-        cache: true,
-    
-        success: function (data, status, xhr) {
-          for (let i = 0; i < data.length; i++) {
-            if (data[i]['username'] !== null ) {
-              const user = {
-                nomor: nomor++,
-                username: data[i]['username'],
-               // fullname: data[i]['fullname'],
-              };
-              users.push(user);
-            }
-          }
-        },
-        error: function (xhr, status, error) {
-          console.log(xhr.responseText);
-        }
-      });
-    
-      i++;
-      if (i == 500) {
-        boo = false;
       }
-    }
-    
-    // Jumlah akun adalah panjang (length) dari array users
-    const jumlahAkun = users.length;
-    console.log("Jumlah akun:", jumlahAkun);
-    
-    // Menampilkan jumlah akun pada elemen dengan id "jumlah-akun" (misalnya pada elemen <span>)
-    $('#jumlah-akun').text(jumlahAkun);
+      $('#jumlah-matkul').text(matkul.length);
 
+      updateTime();
+
+    },
+
+    error: function (xhr, status, error) {
+      // penanganan kesalahan saat permintaan AJAX gagal
+      console.log('Error:', error);
+    }
+  });
 }
 
 function updateTime() {
-    // waktu saat ini
-    const currentTime = new Date();
+  // waktu saat ini
+  const currentTime = new Date();
 
-    // Menambahkan 1 jam
-    const updateDateTime = new Date(currentTime.getTime() + (1000 * 60 * 60));
+  // Menambahkan 1 jam
+  const updateDateTime = new Date(currentTime.getTime() + (1000 * 60 * 60));
 
-    // Selisih waktu dalam milidetik
-    let elapsedTime = updateDateTime - currentTime;
+  // Selisih waktu dalam milidetik
+  let elapsedTime = updateDateTime - currentTime;
 
-    // menghitung jam dan menit yang berlalu
-    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    const minutes = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60));
+  // menghitung jam dan menit yang berlalu
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  const minutes = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60));
 
-    let elapsedTimeString = '';
+  let elapsedTimeString = '';
 
-    if (hours === 1 && minutes === 0) {
-        elapsedTimeString = hours + ' jam yang lalu';
-    } else {
-        elapsedTimeString = minutes + ' menit yang lalu';
-    }
+  if (hours === 1 && minutes === 0) {
+    elapsedTimeString = hours + ' jam yang lalu';
+  } else {
+    elapsedTimeString = minutes + ' menit yang lalu';
+  }
 
-    // memperbarui teks dengan keterangan pembaruan data
-    $('#last-updated').text("Pembaruan data terjadi " + elapsedTimeString);
+  // memperbarui teks dengan keterangan pembaruan data
+  $('#last-updated').text("Pembaruan data terjadi " + elapsedTimeString);
 }
-    
+
 
 $(document).ready(function () {
-    updateData();
-    updateAkun();
+  updateData();
+  updateAkun();
 
-    // Jadwalkan pembaruan data sesuai dengan waktu pembaruan berikutnya setiap 1 jam
-    const intervalID = setInterval(updateData, 60 * 60 * 1000);
+  // Jadwalkan pembaruan data sesuai dengan waktu pembaruan berikutnya setiap 1 jam
+  const intervalID = setInterval(updateData, 60 * 60 * 1000);
 });
