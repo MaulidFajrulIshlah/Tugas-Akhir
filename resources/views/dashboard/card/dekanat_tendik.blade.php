0581f1006a28d2b3da4df83713658bc0abb3af1c
Dila244 <dila.nabilah244@gmail.com>
2023-07-31 11:09:25 +0700

    Add Jumlah Matkul, Role Dekanat-Tendik  FK and FKG

--- resources/views/dashboard/card/dekanat_tendik.blade.php
+++ resources/views/dashboard/card/dekanat_tendik.blade.php
@@ -116,4 +116,50 @@
     <script src="{{ asset('js/daftarMatkul/dekanat_tendik_prodi/dekanat_tendik_prodi_fpsi.js') }}"></script>
     @endcan
 
+    @can('dekanat-tendik-fk')
+    <!-- Daftar Matkul Card -->
+    <div class="col-xl-4 col-md-6 col-11 col-lg-5">
+        <div class="card">
+            <div class="card-body">
+                <div class="ps-1">
+                    <div class="header-card">
+                        <img src="{{ asset('images/book.png') }}" style="margin-top: 3px;">
+                        <h5 class="card-title fw-bold">Daftar Mata Kuliah</h5>
+                    </div>
+                    <h5 id="jumlah-matkul" class="fw-bold mt-1"></h5>
+                    <h6 class="small pt-1">Yang terdapat di LAYAR</h6>
+                    <div class="card-footer bg-transparent mt-3 ps-0">
+                        <small class="text-danger"><span id="last-updated"></span></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End Mahasiswa Card -->
+
+    <script src="{{ asset('js/daftarMatkul/dekanat_tendik_prodi/dekanat_tendik_fk.js') }}"></script>
+    @endcan
+
+    @can('dekanat-tendik-fkg')
+    <!-- Daftar Matkul Card -->
+    <div class="col-xl-4 col-md-6 col-11 col-lg-5">
+        <div class="card">
+            <div class="card-body">
+                <div class="ps-1">
+                    <div class="header-card">
+                        <img src="{{ asset('images/book.png') }}" style="margin-top: 3px;">
+                        <h5 class="card-title fw-bold">Daftar Mata Kuliah</h5>
+                    </div>
+                    <h5 id="jumlah-matkul" class="fw-bold mt-1"></h5>
+                    <h6 class="small pt-1">Yang terdapat di LAYAR</h6>
+                    <div class="card-footer bg-transparent mt-3 ps-0">
+                        <small class="text-danger"><span id="last-updated"></span></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End Mahasiswa Card -->
+
+    <script src="{{ asset('js/daftarMatkul/dekanat_tendik_prodi/dekanat_tendik_fkg.js') }}"></script>
+    @endcan
+
 @endsection
