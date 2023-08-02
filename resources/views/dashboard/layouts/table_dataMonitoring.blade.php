fb466970957dd7e2cb4293dd2d7c485459434673
Dila244 <dila.nabilah244@gmail.com>
2023-08-02 19:50:37 +0700

    Add Keterangan Kegiatan Belajar

--- resources/views/dashboard/layouts/table_dataMonitoring.blade.php
+++ resources/views/dashboard/layouts/table_dataMonitoring.blade.php
@@ -29,3 +29,11 @@
     </thead>
     <tbody></tbody>
 </table>
+
+<div class="alert alert-primary mt-4" role="alert">
+		<div class="col-sm-1 text-center">
+            <i class="fa fa-info"></i> 
+        </div>
+        <div class="col-sm-11">
+        Kegiatan Belajar dihitung apabila dalam course tersebut minimal terdapat 1 resources (file, link, video/audio) dan 1 activity (tugas atau kuis.)
+</div>
